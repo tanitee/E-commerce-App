5d2682a3228c4510aaed0e1026285aadd4d53e26
tanitee <tanitee13@gmail.com>
2025-09-18 22:43:20 +0100

    custom error page created

--- stylemart/src/pages/NotFound.jsx
+++ stylemart/src/pages/NotFound.jsx
@@ -1,8 +1,17 @@
-import React from 'react'
+import { FaExclamationTriangle } from "react-icons/fa";
+import { Link } from 'react-router-dom'
+import '../styles/404.css'
 
 const NotFound = () => {
   return (
-    <div>NotFound</div>
+   <section className="not-found">
+    <FaExclamationTriangle className="not-found-icon" />
+    <h1 className="not-found-title">404 Not Found</h1>
+    <p className="not-found-text">This page does not exist</p>
+    <Link to="/" className="not-found-link">
+      Go Back
+    </Link>
+   </section>
   )
 }
 
