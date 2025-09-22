982c59bbc2e6be47121febf2a0960d76bcdeed98
tanitee <tanitee13@gmail.com>
2025-09-22 16:57:02 +0100

    improved  responsiveness of the navbar and product card on mobile devices

--- stylemart/src/components/Navbar.jsx
+++ stylemart/src/components/Navbar.jsx
@@ -1,11 +1,12 @@
-import {NavLink} from 'react-router-dom'
+import {NavLink , Link} from 'react-router-dom'
 import logo from '../assets/images/stylemart-logo-transparent.png'
 import '../styles/Navbar.css'
 import { IoCartOutline } from "react-icons/io5";
-import { useContext } from 'react';
+import { useContext , useState } from 'react';
 import { CartContext } from '../context/CartContext';
 
 const Navbar = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
   const { cart } = useContext(CartContext);
   const cartItemCount = cart.reduce((total, item) => total + item.quantity, 0);
 
@@ -41,6 +42,24 @@ const Navbar = () => {
           <span className="cart-count">{cartItemCount}</span>
         </NavLink>
       </div>
+
+      {/* <div className="mobile-icons">
+        <Link to="/cart">
+          <IoCartOutline size={28} className='cart-icon' />
+          <span className="cart-count">{cartItemCount}</span>
+        </Link>
+        <span
+          className="hamburger"
+          onClick={() => setMenuOpen((prev) => !prev)}
+        >
+          ☰
+        </span>
+      </div>
+
+      <div className={`dropdown ${menuOpen ? "active" : ""}`}>
+        <Link to="/" onClick={() => setMenuOpen(false)}>Home</Link>
+        <Link to="/products" onClick={() => setMenuOpen(false)}>Products</Link>
+      </div> */}
     </nav>
 
   );
